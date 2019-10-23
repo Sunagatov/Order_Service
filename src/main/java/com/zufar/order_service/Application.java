186dfc707af18a6dc0af1a05a6dfbfd4c48d4460
Сунагатов Зуфар Рустамович <sunagatovzr@vdcom.ru>
2019-10-23 23:58:16 +0400

    Initial commit

---
+++ src/main/java/com/zufar/order_service/Application.java
@@ -0,0 +1,42 @@
+package com.zufar.order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@EnableEurekaClient
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build().apiInfo(apiEndPointsInfo());
+    }
+    
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder()
+                .title("Order REST API")
+                .description("Order REST API provides api for working with orders")
+                .contact(new Contact("Zufar Sunagatov", "https://vk.com/person99", "zufar.sunagatov@gmail.com"))
+                .version("1.0.0")
+                .build();
+    }
+}
