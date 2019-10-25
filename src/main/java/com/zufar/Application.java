8958b5cdc685754d61b7901fb270e85f27aeb45d
Сунагатов Зуфар Рустамович <sunagatovzr@vdcom.ru>
2019-10-25 12:48:44 +0400

    Swagger config and Application classes was fixed.

--- src/main/java/com/zufar/Application.java
+++ src/main/java/com/zufar/Application.java
@@ -3,18 +3,7 @@ package com.zufar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-@EnableSwagger2
 @EnableEurekaClient
 @SpringBootApplication
 public class Application {
