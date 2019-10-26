d160f2fd643bfbffe3be4e09e99a64a1b10d9ae3
Сунагатов Зуфар Рустамович <sunagatovzr@vdcom.ru>
2019-10-26 14:32:42 +0400

    Order service was fixed.

--- src/main/java/com/zufar/service/OrderService.java
+++ src/main/java/com/zufar/service/OrderService.java
@@ -1,20 +1,19 @@
 package com.zufar.service;
 
-import com.zufar.entity.Category;
-import com.zufar.exception.OrderNotFoundException;
 import com.zufar.entity.Order;
 import com.zufar.dto.OrderDTO;
+import com.zufar.entity.Category;
 import com.zufar.repository.OrderRepository;
+import com.zufar.exception.OrderNotFoundException;
 
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Set;
 
 @Service
 @Transactional
@@ -36,25 +35,25 @@ public class OrderService {
         List<Order> orders;
         try {
             orders = (List<Order>) this.orderRepository.findAll();
-            LOGGER.info("All orders were loaded from a database.");
         } catch (Exception exception) {
-            String databaseErrorMessage = "It is impossible to get all clients. There are some problems with a database.";
+            String databaseErrorMessage = "It is impossible to get all orders. There are some problems with a database.";
             LOGGER.error(databaseErrorMessage, exception);
             throw exception;
         }
+        LOGGER.info("All orders were loaded from a database.");
         return orders;
     }
 
-    public List<Order> getAllByIds(Set<Long> ids) {
+    public List<Order> getAllByIds(List<Long> ids) {
         List<Order> orders;
         try {
             orders = (List<Order>) this.orderRepository.findAllById(ids);
-            LOGGER.info("All orders were loaded from a database.");
         } catch (Exception exception) {
-            String databaseErrorMessage = "It is impossible to get all clients. There are some problems with a database.";
+            String databaseErrorMessage = String.format("It is impossible to get orders with ids=[%s]. There are some problems with a database.", ids);
             LOGGER.error(databaseErrorMessage, exception);
             throw exception;
         }
+        LOGGER.info("All orders were loaded from a database.");
         return orders;
     }
 
@@ -62,12 +61,12 @@ public class OrderService {
         List<Order> orders;
         try {
             orders = (List<Order>) this.orderRepository.findAllByClientId(clientId);
-            LOGGER.info(String.format("All orders of the client with id=[%d] were loaded from a database.", clientId));
         } catch (Exception exception) {
             String errorMessage = String.format("It is impossible to get orders of the client with id=[%d]. There are some problems with a database.", clientId);
             LOGGER.error(errorMessage, exception);
             throw exception;
         }
+        LOGGER.info(String.format("All orders of the client with id=[%d] were loaded from a database.", clientId));
         return orders;
     }
 
@@ -76,54 +75,64 @@ public class OrderService {
         Order order;
         try {
             order = this.orderRepository.findById(id).orElse(null);
-            LOGGER.info(String.format("The order with id=[%d] was loaded from a database.", id));
         } catch (Exception exception) {
-            String errorMessage = String.format("It is impossible to get the client with id = [%d]. There are some problems with a database.", id);
+            String errorMessage = String.format("It is impossible to get the order with id = [%d]. There are some problems with a database.", id);
             LOGGER.error(errorMessage, exception);
             throw exception;
         }
+        LOGGER.info(String.format("The order with id=[%d] was loaded from a database.", id));
         return order;
     }
 
     public Order save(OrderDTO order) {
+        Order orderEntity = this.convertToOrder(order);
         Order result;
         try {
-            Category category = this.categoryService.getById(order.getCategoryId());
-            Order orderEntity = this.convertToOrder(order, category);
             result = this.orderRepository.save(orderEntity);
-            LOGGER.info(String.format("The order [%s] was saved in a database.",result));
         } catch (Exception exception) {
             String errorMessage = String.format("It is impossible to save the order - [%s]. There are some problems with a database.", order);
             LOGGER.error(errorMessage, exception);
             throw exception;
         }
+        LOGGER.info(String.format("The order [%s] was saved in a database.", result));
         return result;
     }
 
     public Order update(OrderDTO order) {
         this.isExists(order.getId());
+        Order orderEntity = this.convertToOrder(order);
         Order result;
         try {
-            Category category = this.categoryService.getById(order.getCategoryId());
-            Order orderEntity = this.convertToOrder(order, category);
             result = this.orderRepository.save(orderEntity);
-            LOGGER.info(String.format("The order [%s] was updated in a database.", result));
         } catch (Exception exception) {
             String errorMessage = String.format("It is impossible to updated the order - [%s]. There are some problems with a database.", order);
             LOGGER.error(errorMessage, exception);
             throw exception;
         }
+        LOGGER.info(String.format("The order [%s] was updated in a database.", result));
         return result;
     }
 
     public void deleteById(Long id) {
         this.isExists(id);
-        this.orderRepository.deleteById(id);
+        try {
+            this.orderRepository.deleteById(id);
+        } catch (Exception exception) {
+            String errorMessage = String.format("It is impossible to delete the order with id=[%d]. There are some problems with a database.", id);
+            LOGGER.error(errorMessage, exception);
+            throw exception;
+        }
         LOGGER.info(String.format("The order with id=[%d] was deleted from a database.", id));
     }
 
     public void deleteAllByClientId(Long clientId) {
-        this.orderRepository.deleteAllByClientId(clientId);
+        try {
+            this.orderRepository.deleteAllByClientId(clientId);
+        } catch (Exception exception) {
+            String errorMessage = String.format("It is impossible to delete orders with client id=[%d]. There are some problems with a database.", clientId);
+            LOGGER.error(errorMessage, exception);
+            throw exception;
+        }
         LOGGER.info(String.format("The orders with client id=[%d] was deleted from a database.", clientId));
     }
 
@@ -135,7 +144,8 @@ public class OrderService {
         }
     }
 
-    private Order convertToOrder(OrderDTO orderDTO, Category category) {
+    private Order convertToOrder(OrderDTO orderDTO) {
+        Category category = this.categoryService.getById(orderDTO.getCategoryId());
         return new Order(
                 orderDTO.getId(),
                 orderDTO.getGoodsName(),
