6c694e50baf498187e59a0a1dab6c6fb2edd9e9a
Сунагатов Зуфар Рустамович <sunagatovzr@vdcom.ru>
2019-11-05 22:02:53 +0400

    Unused import was deleted.

--- src/main/java/com/zufar/controller/OrderController.java
+++ src/main/java/com/zufar/controller/OrderController.java
@@ -10,7 +10,6 @@ import io.swagger.annotations.ApiParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.validation.annotation.Validated;
