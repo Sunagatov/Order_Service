fa1ffeee7d7ba81593ec80af6cd6d0f04568ffb3
Сунагатов Зуфар Рустамович <sunagatovzr@vdcom.ru>
2019-11-10 12:35:53 +0400

    Feign client was renamed and documented.

--- order_service_api/src/main/java/com/zufar/order_service_api/client/OrderFeignService.java
+++ order_service_api/src/main/java/com/zufar/order_service_api/client/OrderFeignService.java
@@ -13,11 +13,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 
 @FeignClient(name = "order-service", path = "orders")
-public interface OrderClientService {
+public interface OrderFeignService {
 
+    /**
+     * Returns all orders with given client ids.
+     *
+     * @param clientIds must not be {@literal null}.
+     * @return the orders with the given client ids.
+     */
     @PostMapping(value = "clients")
     @ResponseBody ResponseEntity<List<OrderDTO>> getAllByClientIds(@RequestBody Long... clientIds);
 
+    /**
+     * Deletes all orders with the given client id.
+     *
+     * @param clientId must not be {@literal null}.
+     * @return the operation info.
+     */
     @DeleteMapping(value = "client/{clientId}")
     @ResponseBody ResponseEntity deleteAllByClientId(@PathVariable(value = "clientId") Long clientId);
 }
